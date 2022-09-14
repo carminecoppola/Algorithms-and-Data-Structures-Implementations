c9c781709be0203320c4187053bf178a09f16aeb
Carmine Coppola <74236426+carminecoppola@users.noreply.github.com>
2022-09-14 15:30:38 +0200

    Create Es7.c

---
+++ Es7.c
@@ -0,0 +1,6 @@
+/*  Si progetti un programma in ambiente Linux che, impiegando  la  libreria  pthread,  
+    crei  un  thread  che  estrae N volte (con N passato da riga di comando) un numero 
+    casuale compreso tra 0 e 10 e ne calcoli il fattoriale  ponendolo  in  una  variabile  
+    condivisa  e  un  thread  che  stampi  a  video  il  fattoriale  calcolato  N  volte 
+    prelevandolo dalla variabile condivisa.
+*/
