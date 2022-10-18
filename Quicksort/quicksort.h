ba93da03e1ac5c6d84ea7ef7b2d5169faebb1937
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-10-19 01:21:50 +0200

    agg

--- Quicksort/quicksort.h
+++ Quicksort/quicksort.h
@@ -14,18 +14,40 @@ class quicksort{
 
 void quicksort::partition(int *a,int s, int f, int *puntatore){
 
-
-    if (a[s]> a[f]){
-        
-        int p1,p2;
-        p1 = a[s];
-        p2 = a[f];
-
-        int i,j,k;
-        i = s + 1;
-        j = f - 1;
-        k = s + 1;
+     if (a[s]< a[f]){
+        swap(&a[s], &a[f]);
     }
+    
+    int p1,p2;
+    p1 = a[s];
+    p2 = a[f];
+
+    int i,j,k;
+    i = s + 1;
+    j = f - 1;
+    k = s + 1;
+
+    while (k <= j){
+        if (a[k] < p1){
+            swap(&a[k],&a[i]);
+            i++;
+        }
+        else if (a[k] > p2){
+            while (a[j] > p2 && k < j){
+                j--;
+            }
+            swap(&a[k],&a[j]);
+            j--;
+
+            if (k < p1){
+                /* code */
+            }
+            
+        }
+        
+        
+    }
+    
     
 }
 
