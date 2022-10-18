c2637380c24dd1e9d30af3aec550c21e2143ab95
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-10-19 00:48:16 +0200

    partition

---
+++ Quicksort/main.cpp
@@ -0,0 +1,20 @@
+/*  Implementare il Quicksort con doppio pivot
+        • Questa variante utilizza un pivot per la parte sinistra dell'array ed
+        un pivot per la parte destra con il vincolo che il pivot sinistro sia
+        minore del pivot destro
+        • Al termine della procedura di partition, l'array sarà diviso in tre parti:
+                • La prima in cui gli elementi sono minori del pivot sinistro
+                • La seconda in cui gli elementi sono maggiori o uguali del
+                  pivot sinistro e minori uguali del pivot destro
+                • La terza in cui gli elementi sono maggiori del pivot destro
+    L'algoritmo viene richiamato ricorsivamente sulle tre parti
+    dell'array
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    return 0;
+}
---
+++ Quicksort/quicksort.h
@@ -0,0 +1,33 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+using namespace std;
+
+class quicksort{
+    private:
+        void static partition();
+        void static swap(int * , int *);
+    public:
+       void static quick();
+
+};
+
+void quicksort::partition(){
+
+
+}
+
+void quicksort::swap(int *a, int *b){
+
+    int *t = a;
+    a = b;
+    b = t;
+
+}
+
+void quicksort::quick(){
+
+
+}
+
+#endif
