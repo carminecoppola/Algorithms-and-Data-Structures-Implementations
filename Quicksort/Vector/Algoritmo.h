eea13bf14dd6b5727fe46af06208095cf8659736
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-10-19 12:47:12 +0200

    vector

--- Quicksort/Vector/Algoritmo.h
+++ Quicksort/Vector/Algoritmo.h
@@ -11,52 +11,58 @@ template <class Item>
 
 class Algoritmo{
     private:
-        static void partition(vector<Item> ,vector<int>::iterator,vector<int>::iterator);
+        static int partition(vector<int>::iterator,vector<int>::iterator);
         static void swap(Item &,Item  &);
     public:
-        static void quicksort(vector<Item> ,vector<int>::iterator ,vector<int>::iterator );
+        static void quicksort(vector<int>::iterator ,vector<int>::iterator );
 };
 
 
-template <class Item> void Algoritmo <Item>::partition(vector<Item> a,vector<int>::iterator l,vector<int>::iterator r ){
+template <class Item> int Algoritmo <Item>::partition(vector<int>::iterator l,vector<int>::iterator r){
 
     typename vector<int>::iterator it;   
     typename vector<int>::iterator jt;  //Scorre l'array
        
 
-    it = a.begin()-1;    //Imposto l'indice inizialmente a SX fuori dall'array 
-    auto pivot = a.end()-1;      //Imposto la posizione del pivot che si trova all'estremo destro
+    it = l-1;    //Imposto l'indice inizialmente a SX fuori dall'array 
+    jt = r-1;
+    auto pivot = *(r-1);   //Imposto la posizione del pivot che si trova all'estremo destro
 
     for (;;)    //Finchè è vero
     {
-        while (*(++it) < pivot){
+        while (*(++it) < pivot)
+            while (pivot < *(--jt))
+                if (jt == l)
+                   break;   
+        if (it >= jt)
+            break;
+            
 
-            while (pivot > *(--jt)){
-                if (jt == l){
-                   break;
-                }
-            if (it >= jt){
-                    break;
-            }
-            swap();  
-            }
-        swap
-        }
+            swap(*it,*jt); 
         
-    }
-    
-
+    }//Fine del FOR
+    swap(*it,*r);
+    return it;
 }
 
 template <class Item> void Algoritmo <Item>::swap(Item &a,Item  &b){
 
-    auto t = *a;
-    *a = *b;
-    *b = *a;
+    auto t = a;
+    a = b;
+    b = a;
 
 }
 
-template <class Item> void Algoritmo <Item>::quicksort(vector<Item> array,vector<int>::iterator l,vector<int>::iterator r){
+template <class Item> void Algoritmo <Item>::quicksort(vector<int>::iterator l,vector<int>::iterator r){
+
+    l = begin();
+    r = end();
+
+    if (r <= 1)
+        return;
+    auto i = partition(l,r);
+    quicksort(l,i-1);  //Porzione prima pivot
+    quicksort(i+1,r); //Porzione dopo pivot
 
 }
 
