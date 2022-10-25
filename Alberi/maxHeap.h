0f9f8ee297a84bd7db1953b3b48f3c9621768ca3
Carmine Coppola <carminecoppola917@gmail.com>
2022-10-25 22:09:43 +0200

    parent

--- Alberi/maxHeap.h
+++ Alberi/maxHeap.h
@@ -1,22 +1,34 @@
 #ifndef MAXHEAP_H
 #define MAXHEAP_H
 
-class maxHeap
-{
-private:
-    /* data */
-public:
-    maxHeap(/* args */);
-    ~maxHeap();
+#include "vector"
+using namespace std;
+
+template <class Item>
+
+class maxHeap{
+    private:
+        int padre(int i){
+            int parent = (i+1)/2;
+            return parent;
+        };
+        int sinistro(int i){
+            int sx = (i*2)+1;
+            return sx;
+        };
+        int destro(int i){
+            int dx = (i*2)+2;
+            return dx;
+        };
+
+        void maxHeapify();
+        void buildMaxHeap();
+    public:
+        void insert();
+        void printArray();
+        void printAsciiTree();
 };
 
-maxHeap::maxHeap(/* args */)
-{
-}
-
-maxHeap::~maxHeap()
-{
-}
 
 
 #endif MAXHEAP_H
