cc9f6789725b655adad889362b659c74aa9bdf95
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-10-29 18:46:34 +0200

    heap

--- Heapsort/main.cpp
+++ Heapsort/main.cpp
@@ -10,8 +10,11 @@ int main(){
 
     vector<int> tree{10,15,22,7,40,3};
     heapsort<int> albero = heapsort<int>(tree);
-
+    
+    cout<<"Albero iniziale:";
+    albero.printArray();
     albero.heapSort();
+    cout<<"Albero dopo HEAPSORT:";
     albero.printArray();
 
     return 0;
