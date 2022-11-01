a7d278b82f91d488591dd6a4151840b4b54dfec7
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-01 18:39:06 +0100

    element

---
+++ Code/Elementi_distinti/elementDistinct.h
@@ -0,0 +1,21 @@
+#ifndef ELEMENT_DISTINCT_H
+#define ELEMENT_DISTINCT_H
+
+class elementDistinct{
+private:
+    /* data */
+public:
+    elementDistinct(/* args */);
+    ~elementDistinct();
+};
+
+elementDistinct::elementDistinct(/* args */)
+{
+}
+
+elementDistinct::~elementDistinct()
+{
+}
+
+
+#endif  //ELEMENT_DISTINCT_H
---
+++ Code/Elementi_distinti/main.cpp
@@ -0,0 +1,12 @@
+/*  Dato un array di n elementi, trovare il massimo numero di
+    elementi distinti dopo aver rimosso k elementi (k<=n)
+    • Esempio: {5,4,4,3,4,6,2,1,2}, per k=2 maxnum=5(5,3,6,2,1),
+      per k=3 maxnum=6(5,4,3,6,2,1)
+*/
+
+#include<iostream>
+
+int main(){
+
+    return 0;
+}
