f3922dc773bce1834b91033f414bdf43fc486b91
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-05 17:54:18 +0100

    inverso

--- Algoritmi_di_Ordinamento/Countingsort/Counting/countingsort.h
+++ Algoritmi_di_Ordinamento/Countingsort/Counting/countingsort.h
@@ -11,6 +11,7 @@ class countingsort{
     public:
         static void counting(vector<int> *);
         static void countingCormen(vector<int> *, vector<int> *);
+        static void countingInverso(vector<int> *);
 };
 
 void countingsort::counting(vector<int> *A){
@@ -101,6 +102,51 @@ void countingsort::countingCormen(vector<int> *A, vector<int> *B){
     
 }
 
+void countingsort::countingInverso(vector<int> *A){
 
+
+    int lenghtA = A->size();
+    int max= A->at(0);
+    int min= A->at(0);
+
+    for (int i = 1; i < lenghtA; i++){
+
+        if (A->at(i) > max){
+            max = A->at(i);
+        }
+        else if (A->at(i) < min){
+            min = A->at(i);
+        }
+    }
+
+    //Costruzione dell'array C* di dimensione max-min+1
+    int lenghtC = max-min+1;
+    int *C = new int[lenghtC]; //Visto dalle slide da verificare
+
+    for (int j = 0; j < lenghtC; j++){
+        C[j] = 0; //Inizializzazione degli elementi di C
+    }
+    
+    for (int k = 0; k < lenghtA; k++){
+        C[A->at(k)-min]++;
+    }
+
+    /*Qui ordinaimo in base alla frequenza (contenuta nel vettore C) degli elementi di A
+      ma lo facciamo partendo dalla fine dell'array C visto che bisogna farlo decrescente;*/
+
+    int k = 0; 
+    //indice per l'array A
+    for (int i = lenghtC-1; i >= 0; i--){
+        while (C[i] > 0){
+            //Scriviamo C[i] volte il valore i+min dell'array A
+            A->at(k) = i+min;
+            k++;
+            C[i]--;
+        }  
+    }
+    
+    delete[]C;
+   
+}
 
 #endif
--- Algoritmi_di_Ordinamento/Countingsort/Counting/main.cpp
+++ Algoritmi_di_Ordinamento/Countingsort/Counting/main.cpp
@@ -9,13 +9,14 @@ using namespace std;
 int main(){
 
     vector<int> numeri{2,5,3,0,2,3,0,3};
+
     cout<<endl<<"Array Iniziale:"<<endl;
     for (auto i : numeri){
        cout<< i <<" ";
     }
     cout<<endl;
-
-
+    
+    //CountingSort Base
     countingsort::counting(&numeri);
     cout<<endl<<"Array dopo CountingSort Base:"<<endl;
     for (auto i : numeri){
@@ -23,16 +24,20 @@ int main(){
     }
     cout<<endl;
     
-    
-
-
-
-    //Cormen
+    //CountingSort Cormen
     vector<int> B(numeri.size());
     countingsort::countingCormen(&numeri,&B);
 
     cout<<endl<<"Array dopo CountingSort Cormen:"<<endl;
     for (auto i : B){
+        cout<< i <<" ";
+    }
+    cout<<endl;
+
+    //CountingSort Inverso
+    countingsort::countingInverso(&numeri);
+    cout<<endl<<"Array dopo CountingSort Inverso:"<<endl;
+    for (auto i : numeri){
         cout<< i <<" ";
     }
     cout<<endl;
