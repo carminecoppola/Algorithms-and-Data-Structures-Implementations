e22dafd701b5d3108708e3573b0479ce0d40d262
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-07 09:04:31 +0100

    commenti

--- Algoritmi_di_Ordinamento/Radixsort/Algoritmo con Counting/radixsort.h
+++ Algoritmi_di_Ordinamento/Radixsort/Algoritmo con Counting/radixsort.h
@@ -9,7 +9,7 @@ using namespace std;
 class Radixsort{
     private:
         static int getMax(vector<int> *A);
-        static void counting(vector<int> *A, int k);
+        static void countingRadix(vector<int> *A, int k);
     public:
         static void radixsort(vector<int> *A);
         static void print(vector<int> );
@@ -27,20 +27,25 @@ int Radixsort::getMax(vector<int> *A){
     return max;   
 }
 
-void Radixsort::counting(vector<int> *A, int k){
+/*  Questo metodo ha lo stesso funzionamento del counting sort, le uniche differenze
+    sono che non prendiamo l'intero numero, ma partiamo dalla cifra meno significativa 
+    fino ad arrivare a quella più significativa, questo lo faremo tramite il divisore (k)
+    che passeremo dal main.
+*/
+void Radixsort::countingRadix(vector<int> *A, int k){
 
     int lenghtA = A->size();
     int output [lenghtA]; //Array di output
     int count[10] = {0}; //Array delle occorrenze inizializzato a zero
 
     //Occorrenze
-    for (int i = 0; i < lenghtA; i++){
-        count[(A->at(i)/k) % 10]++;
+    for (int i = 0; i < lenghtA; i++){       //es. C = 2 0 2 0 1 2 1 0 0 0   
+        count[(A->at(i)/k) % 10]++;         //count[170/1 % 10]++   =>   count[0]++;
     }
     
     //Questo ciclo for ci darà la corretta posizione degli elementi di count
-    for (int i = 1; i < 10; i++){
-        count[i] += count[i-1];
+    for (int i = 1; i < 10; i++){       //es. C = 2 0 2 0 1 2 1 0 0 0
+        count[i] += count[i-1];        //     C = 2 2 4 4 5 7 8 8 8 8
     }
 
     //Questo ciclo for serve per creare l'array di output
@@ -56,14 +61,22 @@ void Radixsort::counting(vector<int> *A, int k){
 
 }
 
+/*  In questo metodo utilizziamo il metodo scritto in precedenza per calcolarci
+    l'elemento massimo "getMax()" che ci servirà per il countingRadix() eseguito
+    in un ciclo for che parte da k = 1 finchè il rapporto tra l'elemento massimo e
+    l'indice sia > 0, inoltre invece di incrementare l'indice lo moltiplichiamo
+    ogni passaggio *10 come spiegato in seguito */
+
 void Radixsort::radixsort(vector<int> *A){
 
     int massimo = getMax(A);    //utilizziamo il metodo getMax() per calcolarci il massimo
+                               //es. A = 170 45 75 90 802 24 2 66    
+                              //max = 802
 
-    for (int k = 1; massimo/k > 0; k *= 10){
-        counting(A,k);
-    }
-    
+    for (int k = 1; massimo/k > 0; k *= 10){        //div = 1;  802/1 => 802 > 0 V
+        countingRadix(A,k);                        //div = 10; 802/10 => 80 > 0 V
+    }                                             //div = 100 802/100 => 8 > 0 V
+                                                 //div = 1000 802/1000 => 0. > 0 X
 }
 
 void Radixsort::print(vector<int> A){
