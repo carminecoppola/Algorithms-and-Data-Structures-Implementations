98b085e4b161edc5be294b044cce754f199e6184
Carmine Coppola <carminecoppola917@gmail.com>
2022-11-08 22:50:57 +0100

    commit

--- Alberi_Binari_Ricerca/BinarySearchTree/binarySearchTree.h
+++ Alberi_Binari_Ricerca/BinarySearchTree/binarySearchTree.h
@@ -10,15 +10,20 @@ using namespace std;
 template<class Item>
 class binarySearchTree{
     private:
-       
+       Nodo<Item> *root;
     public:
-        binarySearchTree(){};
-        int minimumTree( );
+        binarySearchTree();
+        int minimumTree();
         int maximumTree();
         int predecessorTree();
         int successorTree();
         void insertTree();
         void deleteTree();
 };
+
+template<class Item> binarySearchTree::binarySearchTree(){
+    root = nullptr;
+
+}
 
 #endif
