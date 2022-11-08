874d890fcab7b77f7e8546099cab8e6888458c4a
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-08 23:57:22 +0100

    insert

--- Alberi_Binari_Ricerca/BinarySearchTree/binarySearchTree.h
+++ Alberi_Binari_Ricerca/BinarySearchTree/binarySearchTree.h
@@ -14,7 +14,7 @@ class binarySearchTree{
     public:
         binarySearchTree();
 
-        Nodo *getRoot(){
+        Nodo<Item> *getRoot(){
             return root;
         };
 
@@ -22,12 +22,53 @@ class binarySearchTree{
         int maximumTree();
         int predecessorTree();
         int successorTree();
-        void insertTree();
+        void insertTree(Item value);
         void deleteTree();
+        void preOrderVisit(Nodo<Item> *current);
 };
 
 template<class Item> binarySearchTree<Item>::binarySearchTree(){
     root = nullptr;
+}
+
+template<class Item> void binarySearchTree<Item>::insertTree(Item value){
+    Nodo<Item> *nodeToInsert = new Nodo<Item>(value);
+    Nodo<Item> *parent = nullptr;
+    Nodo<Item> *current = root;
+
+    while (current != nullptr)
+    {
+        parent = current;
+        if (nodeToInsert->getInfo() < current->getInfo())
+        {
+            current = current->getLeft();
+        }
+        else{
+            current = current->getRight();
+        }
+        
+    }
+
+    nodeToInsert->setParent(parent);
+
+    if (parent == nullptr){
+        root = nodeToInsert;
+    }
+    else if(nodeToInsert->getInfo() < parent->getInfo()){
+        parent->setLeft(nodeToInsert);
+    }
+    else
+        parent->setRight(nodeToInsert);
+
+}
+
+template<class Item> void binarySearchTree<Item>::preOrderVisit(Nodo<Item> *current){
+
+    if( current != nullptr)
+        cout<<current->getInfo()<<endl;
+
+    binarySearchTree::preOrderVisit(current->getLeft());
+    binarySearchTree::preOrderVisit(current->getRight());
 }
 
 #endif
--- Alberi_Binari_Ricerca/BinarySearchTree/main.cpp
+++ Alberi_Binari_Ricerca/BinarySearchTree/main.cpp
@@ -12,11 +12,26 @@
 
 #include <iostream>
 #include"binarySearchTree.h"
+#include"nodo.h"
 
 using namespace std;
 
 int main(){
 
+    binarySearchTree<int>* tree = new binarySearchTree<int>;
+
+    tree->insertTree(10);
+    tree->insertTree(20);
+    tree->insertTree(2);
+    tree->insertTree(4);
+    tree->insertTree(60);
+    tree->insertTree(222);
+    tree->insertTree(40);
+    tree->insertTree(62);
+
+
+    cout<<"Albero dopo inserimento:"<<endl;
+    tree->preOrderVisit(tree->getRoot());
     
     return 0;
 }
