be6528a39f33aa0fe487d985cd5134f75ff190f5
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-09 00:29:03 +0100

    main

--- Alberi_Binari_Ricerca/BinarySearchTree/main.cpp
+++ Alberi_Binari_Ricerca/BinarySearchTree/main.cpp
@@ -20,7 +20,7 @@ int main(){
 
     binarySearchTree<int> tree;
 
-    tree.insertTree(12);
+    /*tree.insertTree(12);
     tree.insertTree(9);
     tree.insertTree(5);
     tree.insertTree(2);
@@ -28,9 +28,17 @@ int main(){
     tree.insertTree(17);
     tree.insertTree(18);
     tree.insertTree(19);
-    tree.insertTree(15);
+    tree.insertTree(15);*/
 
-    cout<<endl<<"Abero Simmetrico: "<<endl;
+    tree.insertTree(13);
+    tree.insertTree(12);
+    tree.insertTree(16);
+    tree.insertTree(10);
+    tree.insertTree(11);
+    tree.insertTree(14);
+    tree.insertTree(17);
+
+    cout<<endl<<"Abero preOrder: "<<endl;
     tree.preOrderVisit(tree.getRoot());
     
     cout<<endl<<endl<<"Radice: "<<tree.getRoot()->getInfo()<<endl<<endl;
