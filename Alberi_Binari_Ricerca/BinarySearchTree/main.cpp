a321ce2f16a9213c87d8c5e683bd291c030b1920
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-10 00:26:04 +0100

    commit

--- Alberi_Binari_Ricerca/BinarySearchTree/main.cpp
+++ Alberi_Binari_Ricerca/BinarySearchTree/main.cpp
@@ -28,34 +28,35 @@ int main(){
     tree.insertTreeRicors(14,nullptr,tree.getRoot());
     tree.insertTreeRicors(17,nullptr,tree.getRoot());
 
-    cout<<endl<<"Abero Pre-Order: "<<endl;
+    cout<<endl<<"• Albero Pre-Order: "<<endl;
     tree.preOrderVisit(tree.getRoot());
     
-    cout<<endl<<endl<<"Radice: "<<tree.getRoot()->getInfo()<<endl;
+    cout<<endl<<endl<<"  -Radice: "<<tree.getRoot()->getInfo()<<endl;
 
-    cout<<endl<<"Abero In-Order: "<<endl;
-    tree.inOrderVisit(tree.getRoot());
-
-    cout<<endl<<"Elemento trovato: "<<tree.treeSearch(12,tree.getRoot())->getInfo()<<endl;
-
-    cout<<endl<<"Il minimo è: "<<tree.minimumTree(tree.getRoot())->getInfo()<<endl;
-    cout<<endl<<"Il massimo è: "<<tree.maximumTree(tree.getRoot())->getInfo()<<endl;
-
-    auto *x = tree.treeSearch(14,tree.getRoot());
-    cout<<endl<<"Il successore del nodo "<< x->getInfo()<<" è: "<< tree.successorTree(x)->getInfo() <<endl;
-    
-    
-    cout<<endl<<"Il predecessore del nodo "<< x->getInfo()<<" è: "<< tree.predecessorTree(x)->getInfo() <<endl;
-    
-    cout<<endl<<"Il nodo da cancellare è: "<< x->getInfo()<<endl;
-    tree.deleteTree(x);
-
-    cout<<endl<<"Abero Pre-Order: "<<endl;
-    tree.preOrderVisit(tree.getRoot());
-
-    cout<<endl<<"Abero In-Order: "<<endl;
+    cout<<endl<<"• Albero In-Order: "<<endl;
     tree.inOrderVisit(tree.getRoot());
     cout<<endl;
+
+    cout<<endl<<"  -Elemento trovato: "<<tree.treeSearch(12,tree.getRoot())->getInfo()<<endl;
+
+    cout<<endl<<"  -Il minimo è: "<<tree.minimumTree(tree.getRoot())->getInfo()<<endl;
+    cout<<endl<<"  -Il massimo è: "<<tree.maximumTree(tree.getRoot())->getInfo()<<endl;
+
+    auto *x = tree.treeSearch(14,tree.getRoot());
+    cout<<endl<<"  -Il successore del nodo "<< x->getInfo()<<" è: "<< tree.successorTree(x)->getInfo() <<endl;
     
+    
+    cout<<endl<<"  -Il predecessore del nodo "<< x->getInfo()<<" è: "<< tree.predecessorTree(x)->getInfo() <<endl;
+    
+    cout<<endl<<"  -Il nodo da cancellare è: "<< x->getInfo()<<endl;
+    tree.deleteTree(x);
+
+    cout<<endl<<"• Albero Pre-Order: "<<endl<<endl;
+    tree.preOrderVisit(tree.getRoot());
+
+    cout<<endl<<endl<<"• Albero In-Order: "<<endl<<endl;
+    tree.inOrderVisit(tree.getRoot());
+    cout<<endl;
+
     return 0;
 }
