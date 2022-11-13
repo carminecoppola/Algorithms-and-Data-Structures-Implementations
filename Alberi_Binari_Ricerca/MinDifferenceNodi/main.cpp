0defe6061e493b9c49d9eb272cad0964a73b3e19
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-13 17:17:24 +0100

    commit

--- Alberi_Binari_Ricerca/MinDifferenceNodi/main.cpp
+++ Alberi_Binari_Ricerca/MinDifferenceNodi/main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include<vector>
+#include<limits.h>
 #include "binarySearchTree.h"
 
 using namespace std;
@@ -19,57 +20,48 @@ void arrayVisit(Nodo<Item> *current, vector<Item> *array){
 };
 
 template<class Item>
-Item differenza(vector<Item> array){
+Item differenzaRic(vector<Item> array, int i, int diff,int min){
 
-    Item indice = 0;
-    Item diff = abs(array.at(0) - array.at(1));
 
-    for (int i = 1; i < array.size()-1; i++){
-        if (abs(array.at(i) - array.at(i+1)) < diff){
-            return i;
-        }
-    } 
-    return 0;
-};
-
-template<class Item>
-Item differenzaRic(vector<Item> array, int i, int diff){
-
-    int min = 0;
     if (i == array.size()-1)
         return min; 
-    if (abs(array.at(i) - array.at(i+1)) < diff)
-        min = i;
 
-    return differenzaRic(array,i+1,abs(array.at(i) - array.at(i+1)));
+    if (abs(array.at(i) - array.at(i+1)) <= diff){
+
+        diff = abs(array.at(i) - array.at(i+1));
+        return differenzaRic(array,i+1,diff,i);
+    }
+    else{
+        return differenzaRic(array,i+1,diff,min);
+    }
 }
 
 int main(){
 
     binarySearchTree<int> tree;
-    vector<int> array;
 
-    tree.insert(15);
-    tree.insert(12);
-    tree.insert(16);
-    tree.insert(10);
-    tree.insert(11);
-    tree.insert(14);
-    tree.insert(17);
-
-    cout << endl << "• Albero Pre-Order: " << endl;
-    tree.preOrderVisit(tree.getRoot());
-    
-    cout << endl << endl << "  -Radice: "<<tree.getRoot()->getInfo()<<endl;
+    tree.insert(3);
+    tree.insert(24);
+    tree.insert(13);
+    tree.insert(9);
+    tree.insert(18);
+    tree.insert(1);
+    tree.insert(21);
 
     cout<<endl<<"• Albero In-Order: "<<endl;
     tree.inOrderVisit(tree.getRoot());
     cout<<endl;
 
+    vector<int> array;
     arrayVisit(tree.getRoot(),&array);
 
-    int figlio = differenzaRic(array,0,UINT16_MAX);
-    cout << "I nodi minimi sono " << array.at(figlio) << " e " << array.at(figlio+1) << endl;
+    cout<<endl<<"Vector: ";
+    for(auto i:array)
+        cout<<i<<" ";
+    cout<<endl;
+
+    int ind = differenzaRic(array, 0, UINT16_MAX,0);
+    cout << "I nodi minimi sono " << array.at(ind) << " e " << array.at(ind+1) << endl;
 
     return 0;
 }
