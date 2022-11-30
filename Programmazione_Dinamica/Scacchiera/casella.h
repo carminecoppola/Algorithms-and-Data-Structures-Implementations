56ed676b528503f41210296a16466bbaf9affaf3
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-30 23:29:45 +0100

    Scacchiera

--- Programmazione_Dinamica/Scacchiera/casella.h
+++ Programmazione_Dinamica/Scacchiera/casella.h
@@ -1,16 +1,56 @@
-
 #include<iostream>
 
 using namespace std;
 
+/*  Creiamo una classe di tipo ENUM per gestire i tre diversi casi della
+    direzione che viene fatta ad ogni casella.*/
+
+enum class Direction{UP,LEFT,DIAG};
+ostream& operator<<(ostream& out, const Direction& dir){
+    switch (dir)
+    {
+    case Direction::UP:
+        out<<"U";
+        break;
+    case Direction::LEFT:
+        out<<"L";
+        break;
+    case Direction::DIAG:
+        out<<"D";
+        break;
+    default:
+        break;
+    }
+    return out;
+}
+
+
 class casella{
     private:
-        /* data */
+        int value;
+        Direction dir;  //per indicare la direzione che assume ad ogni passo la pedina
     public:
-        casella(/* args */);
-        ~casella();
+        casella():value{0}, dir{Direction::UP}{}
+        casella(int value, Direction dir):value{value}, dir{dir}{}
+
+        //metodo di getter per ritornare il valore e la direzione
+
+        int getValue(){
+            return value;
+        };
+
+        Direction getDirection(){
+            return dir;
+        };
+
+        //Metodi per settare il valore e la direzione
+
+        void setValue(int value){
+            this->value = value;
+        }
+
+        void setDirection(Direction dir){
+            this->dir = dir;
+        }
 };
 
-casella::casella(/* args */){}
-
-casella::~casella(){}
--- Programmazione_Dinamica/Scacchiera/main.cpp
+++ Programmazione_Dinamica/Scacchiera/main.cpp
@@ -9,12 +9,125 @@
 */
 
 #include <iostream>
+#include<iomanip>
 #include"casella.h"
+#define n 5
 
 using namespace std;
 
 int main(){
 
+    //Creiamo la nostra scacchiera (matrice[n][n])
+    int scacchiera[n][n] = {6, 7, 4, 7, 8,
+                            7, 6, 1, 1, 4,
+                            3, 5, 7, 8, 2,
+                            2, 6, 7, 0, 2,
+                            7, 3, 5, 6, 1 };
+
+    //Quest'altra matrice ci servira per i valori dei percorsi
+    casella scacchiera2[n][n];
+
+    int i,j,m;    // i = indice per le righe, j = indice per le colonne , m = indice del valore massimo;
+
+    //Inizializziamo la secodna scacchira
+
+    for (i = 0; i < n; i++){
+        for (j = 0;j < n; j++){
+            scacchiera2[i][j].setValue(0);
+        } 
+    }
+    
+    //Sottoproblema della prima casella (6,in alto a sx)
+
+    scacchiera2[0][0].setValue(scacchiera[0][0]);
+    
+    //Prima riga
+    i = 0;
+    
+    for (j = 1; j < n; j++) //partiamo da 1 poichè la prima posizione gia l'abbiamo settata
+    {
+        scacchiera2[i][j].setValue(scacchiera2[i][j-1].getValue() + scacchiera[i][j]);
+        scacchiera2[i][j].setDirection(Direction::LEFT);    //Andiamo sempre a sx
+    }
+    
+    //Prima colonna
+    j = 0;
+    for (i = 1; i < n; i++)
+    {
+        scacchiera2[i][j].setValue(scacchiera2[i-1][j].getValue() + scacchiera[i][j]);
+        scacchiera2[i][j].setDirection(Direction::UP);    //Andiamo sempre in alto
+    }
+    
+
+    //Resto dei casi
+
+    Direction d;
+
+    for (i = 1; i < n; i++)
+    {
+        for (j = 1; j < n; j++)
+        {
+            /*Il massimo sarà il nostro elemento in diagonale 
+                            | 6  7 |->13
+                         13 | 7  6 |->19    */
+
+            m = scacchiera2[i-1][j-1].getValue();
+            d = Direction::DIAG;
+
+            //Se l'elemento maggiore è quello in alto setto UP
+
+            if (scacchiera2[i-1][j].getValue() > m)
+            {
+                m = scacchiera2[i-1][j].getValue();
+                d = Direction::UP;
+            }
+            
+            //Se l'elemento maggiore è quello a Sinistra setto LEFT
+
+            if (scacchiera2[i][j-1].getValue() > m)
+            {
+                m = scacchiera2[i][j-1].getValue();
+                d = Direction::LEFT;
+            }
+            
+            scacchiera2[i][j].setValue(m + scacchiera[i][j]);
+            scacchiera2[i][j].setDirection(d);
+
+        }
+    }
+    
+
+    //stampa matrice
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            cout<<setw(7)<<scacchiera2[i][j].getValue()<<" ("<<scacchiera2[i][j].getDirection()<<")";
+        }
+        cout<<endl;
+    }
+
+    //stampa percorso
+    i = n-1;
+    j = n-1;
+    cout<< i <<" "<< j <<endl;
+    while(i != 0 or j !=0){
+        d = scacchiera2[i][j].getDirection();
+        switch (d)
+        {
+        case Direction::UP:
+            i--;
+            break;
+        case Direction::LEFT:
+            j--;
+            break;
+        case Direction::DIAG:
+            i--;
+            j--;
+            break;
+        default:
+            break;
+        }
+        cout<<i<< " "<<j<<endl;
+    }
 
     return 0;
 }
