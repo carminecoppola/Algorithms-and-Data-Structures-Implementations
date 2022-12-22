dad2b63fa25f20965e148384c81aa4b77334d2bb
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-12-22 10:34:49 +0100

    new

--- 01.PreparazioneProvaImpronta/QuickSort/algoritmo.h
+++ 01.PreparazioneProvaImpronta/QuickSort/algoritmo.h
@@ -11,16 +11,53 @@ class algoritmo
 {
 private:
 
-    void swap(int &a,int &b);
-    void static partition();
+    static void swap(T &a,T &b);
+    static typename vector<T>::iterator partition(typename vector<T>::iterator l,typename vector<T>::iterator r);
 
 public:
 
-    void static quickSort();
+    void static quickSort(typename vector<T>::iterator l, typename vector<T>::iterator r);
 };
 
-template<class T> void algoritmo<T>::swap(int &a,int &b){
+template<class T> void algoritmo<T>::swap(T &a,T &b){
 
+    T temp = a;
+    a = b;
+    b = temp;
+    
+}
+
+template <class T> typename vector<T>::iterator algoritmo <T>::partition(typename vector<T>::iterator l,typename vector<T>::iterator r){
+
+    typename vector<T>::iterator i; 
+    typename vector<T>::iterator j;
+    
+    i = l-1;
+    j = r; 
+
+    T pivot = *r;
+
+    for(;;){
+
+        while (*(++i) < pivot);
+        while (pivot < *(--j))
+            if (j == l)
+                break;
+        if (i >= j)
+            break;
+        swap(*i, *j);
+    }
+    swap(*i,*r);
+    return i;
+}
+
+template<class T> void algoritmo<T>::quickSort(typename vector<T>::iterator l, typename vector<T>::iterator r){
+
+    if (r <= l)
+        return;
+    auto i = partition(l,r);
+    quickSort(l,i-1); 
+    quickSort(i+1,r); 
 }
 
 #endif
--- 01.PreparazioneProvaImpronta/QuickSort/main.cpp
+++ 01.PreparazioneProvaImpronta/QuickSort/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include<vector>
+#include"algoritmo.h"
+
 using namespace std;
 
 int main(){
 
-    
+    vector<int> a = {7,9,3,1};
+   
+    cout<<"Array iniziale:"<<endl;
+        for (auto i:a){
+            cout<<i<<" ";
+        }
+    cout<<endl;
+
+    //Chiamata alla funzione mergesort
+    algoritmo<int>::quickSort(a.begin(),a.end()-1);
+
+    cout<<"Array ordinato dopo mergesort:"<<endl;
+    for (auto i:a){
+        cout<<i<<" ";
+    }
 
     return 0;
 }
