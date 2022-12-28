69bcf8cd015cff5869f17e3e8cf67cc6381995ea
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-12-28 12:54:11 +0100

    new

---
+++ 01.PreparazioneProvaImpronta/Heapsort/MinHeap/algoritmo.h
@@ -0,0 +1,110 @@
+#ifndef ALGORITMO_H
+#define ALGORITMO_H
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template<class T>
+class algoritmo
+{
+    private:
+        int heapSize;
+        
+        int left(int i){return (i*2)+1;}
+        int right(int i){return (i*2)+2;}
+
+    protected:
+        vector<int> tree;
+        int parent(int i){return (i-1)/2;}
+        void swap(T &a, T&b);
+
+        void setHeapSize(int size){heapSize = size;}
+        int getHeapSize(){return heapSize;}
+        vector<T> getTree(){return tree;}
+
+        void minHeapify(int);
+    public:
+        algoritmo(vector<T> );
+        algoritmo();
+
+        void buildMinHeap();
+        void insert(T nodo);
+        void heapSort();
+        void printArray();
+};
+
+//Costruttore
+template<class T> algoritmo<T>::algoritmo(vector<T> x)
+{
+    tree = x;
+    heapSize = x.size();
+}
+//Secondo Costruttore
+template<class T> algoritmo<T>::algoritmo()
+{
+    heapSize = 0;
+}
+//SWAP
+template<class T> void algoritmo<T>::swap(T &a, T&b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+//MINHEAPIFY
+template<class T> void algoritmo<T>::minHeapify(int i)
+{
+    int min = i;
+    int l = left(i);
+    int r = right(i);
+
+    if(l < getHeapSize() && getTree().at(min) > getTree().at(l))
+        min = l;
+    if(r < getHeapSize() && getTree().at(min) > getTree().at(r))
+        min = r;
+    if(min != i){
+        swap(tree.at(i), tree.at(min));
+        minHeapify(min);
+    }
+}
+//BUILDMINHEAP
+template<class T> void algoritmo<T>::buildMinHeap()
+{
+    setHeapSize((int) tree.size());
+
+    for (int i = getHeapSize()/2 -1; i >= 0; i--)
+        minHeapify(i);
+}
+//INSERT
+template<class T> void algoritmo<T>::insert(T nodo)
+{
+    setHeapSize(getHeapSize()+ 1);
+    tree.push_back(nodo);
+
+    int i = getHeapSize()-1;
+    while (i > 0 && getTree().at(parent(i)) > nodo)
+    {
+        swap(tree.at(parent(i)), tree.at(i));
+        i = parent(i);
+    }
+}
+//HEAPSORT
+template<class T> void algoritmo<T>::heapSort()
+{
+    buildMinHeap();
+    for(int i = getHeapSize()-1; i >= 0 ; i--)
+    {
+        swap(tree.at(i),tree.at(0));
+        setHeapSize(getHeapSize()-1);
+        minHeapify(0);
+    }
+}
+//PRINTARRAY
+template<class T> void algoritmo<T>::printArray()
+{
+    for(auto i:tree)
+        cout<<i<<" ";
+    cout<<endl;
+}
+#endif
