6e2061429380a8408a69a29fc83a3ad54c7c97fb
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-01-02 18:43:10 +0100

    insert

--- 01. ProvaImpronta/ABR/BinaryTree.h
+++ 01. ProvaImpronta/ABR/BinaryTree.h
@@ -7,22 +7,70 @@
 
 using namespace std;
 
+template<class T>
 class BinaryTree
 {
-private:
-    /* data */
-public:
-    BinaryTree(/* args */);
-    ~BinaryTree();
+    private:
+        Nodo<T> *root;
+
+        void insertRicorsivo(T ,Nodo<T>* , Nodo<T>* );
+        void insertNodo(T ,Nodo<T>* , Nodo<T>* );
+
+        Nodo<T> *findSuccessor(Nodo<T> *);
+        Nodo<T> *findPredecessor(Nodo<T> *);
+        
+    public:
+        BinaryTree(/* args */);
+
+        Nodo<T> *getRoot(){return root;}
+
+        void insert(T);
+        Nodo<T> *treeSearch(Nodo<T> *, T);
+        Nodo<T> *getMinimum(Nodo<T> *);
+        Nodo<T> *getMaximum(Nodo<T> *);
+        Nodo<T> *successor(Nodo<T> *);
+        Nodo<T> *predecessor(Nodo<T> *);
+
+        void visitInOrder(Nodo<T> *);
+        void visitPreOrder(Nodo<T> *);
+        void visitPostOrder(Nodo<T> *);
 };
 
-BinaryTree::BinaryTree(/* args */)
+//Costruttore
+template<class T> BinaryTree <T>::BinaryTree(/* args */)
 {
+    root = nullptr;
 }
 
-BinaryTree::~BinaryTree()
+//Insert
+template<class T> void BinaryTree<T>::insert(T value)
 {
+    insertRicorsivo(value,nullptr,root);
 }
 
+//Insert Ricorsivo
+template<class T> void BinaryTree <T>::insertRicorsivo(T value,Nodo<T> *prev , Nodo<T> *curr)
+{ 
+    if (root = nullptr)
+        root = new Nodo<T>(value);
+    else if(curr = nullptr)
+        insertNodo(value,prev,curr);
+    else if(curr->getInfo() > value)
+        insertRicorsivo(value,curr,curr->getLeft());
+    else
+        insertRicorsivo(value,curr,curr->getRight());
+    
+}
 
+//Insert Nodo
+template<class T> void BinaryTree<T>::insertNodo(T value, Nodo<T> *prev, Nodo<T> *curr)
+{
+    curr = new Nodo<T>(value);
+    curr->setParent(prev);  //Setto il padre che sarà l'elemento precedente
+
+    if(curr->getInfo() > prev->getInfo())   //Se il nodo corrente è maggiore di quello precedente lo metto DX
+        prev->setRight(curr);
+    else
+        prev->setLeft(curr);        
+}
 #endif
--- 01. ProvaImpronta/ABR/main.cpp
+++ 01. ProvaImpronta/ABR/main.cpp
@@ -9,6 +9,15 @@ using namespace std;
 
 int main(){
 
+    BinaryTree<int> ABR;
+
+    ABR.insert(11);
+    ABR.insert(12);
+    ABR.insert(16);
+    ABR.insert(10);
+    ABR.insert(13);
+    ABR.insert(14);
+    ABR.insert(17);
 
     return 0;
 }
