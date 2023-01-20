a02f35390a3c9906c3060258846211b335829a41
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-01-20 12:40:36 +0100

    nodo

---
+++ 01. ProvaImpronta/Grafi/BFS_VISIT/BFS3/Nodo.h
@@ -0,0 +1,38 @@
+#ifndef NODO_H
+#define NODO_H
+
+#include<iostream>
+#include<list>
+#include"Vertice.h"
+
+using namespace std;
+
+template<class T>
+class Nodo
+{
+
+    private:
+        Vertice<T> *vertice;
+        list<Vertice<T>*> listaAdj;
+
+    public:
+        Nodo(Vertice<T> *vertice){this->vertice = vertice;}
+
+        //Metodo di get
+        Vertice<T> *getVertice(){return vertice;}
+        list<Vertice<T>*> getList(){return listaAdj;}
+        void append(Vertice<T> *vertice){listaAdj.push_back(vertice);}
+
+
+        friend ostream &operator<<(ostream &out, const Nodo<T> &obj)
+        {
+            out<<*obj.vertice<<"-->";
+            for(auto i:obj.listaAdj)
+                out<<*i<<"-->";
+            out<<"NULL";
+            return out;
+        }
+
+};
+
+#endif
