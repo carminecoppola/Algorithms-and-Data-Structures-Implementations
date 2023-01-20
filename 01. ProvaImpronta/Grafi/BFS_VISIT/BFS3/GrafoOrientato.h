64cb2035e9a0c16ac4a9a9e9facc8b69ad434868
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-01-20 14:58:57 +0100

    bfs

---
+++ 01. ProvaImpronta/Grafi/BFS_VISIT/BFS3/GrafoOrientato.h
@@ -0,0 +1,86 @@
+#ifndef GRAFOORIENTATO_H
+#define GRAFOORIENTATO_H
+
+#include<iostream>
+#include<limits>
+#include<stack>
+#include<queue>
+#include<list>
+#include"Nodo.h"
+#include"Vertice.h"
+
+using namespace std;
+
+template<class T>
+class GrafoOrientato
+{
+    private:
+        vector<Nodo<T>> grafo;
+        list<Vertice<T>*> getListAdj(Vertice<T> *);
+    public:
+
+        void addNodo(Nodo<T> nodo){grafo.push_back(nodo);}
+        void addArco(int i, Vertice<T> *vertice){grafo.at(i).append(vertice);}
+
+        void BFS(Vertice<T> *sorgente);
+
+        friend ostream &operator<<(ostream &out, GrafoOrientato<T> &obj)
+        {
+            for(auto i:obj.grafo)
+                out<<i<<endl;
+            return out;
+        }
+};
+
+template<class T> list<Vertice<T>*> GrafoOrientato<T>::getListAdj(Vertice<T> *vertice)
+{
+    for(auto i:grafo)
+    {
+        if(i.getVertice() == vertice)
+            return i.getList();
+        
+    }
+    return grafo.at(0).getList();
+}
+
+template<class T> void GrafoOrientato<T>::BFS(Vertice<T> *sorgente)
+{
+
+    for(u:grafo)
+    {
+        u.getVertice()->setColor(Color::WHITE);
+        u.getVertice()->setPredecessore(nullptr);
+        u.getVertice()->setDistanza(UINT16_MAX);
+    }
+
+    sorgente->setColor(Color::WHITE);
+    sorgente->setPredecessore(nullptr);
+    sorgente->setDistanza(0);
+
+    queue<Vertice<T>*> q;
+    q.push(sorgente);
+
+    while(!q.empty())
+    {
+        auto u = q.front();
+        q.pop();
+
+        list<Vertice<T>*> adj = getListAdj(u);
+
+        for(auto v:adj)
+        {
+            if(v->getColor() == Color::WHITE)
+            {
+                v->setColor(Color::GRAY);
+                v->setPredecessore(u);
+                v->setDistanza(u->getDistanza()+1);
+                q.push();
+            }
+        }
+
+        u->setColor(Color::BLACK);
+    }
+}
+
+
+#endif
