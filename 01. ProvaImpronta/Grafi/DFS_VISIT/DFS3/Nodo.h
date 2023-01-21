f9daec8348d6c077696efb1aa666bfde7636d4dc
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-01-21 12:32:07 +0100

    Nodo

---
+++ 01. ProvaImpronta/Grafi/DFS_VISIT/DFS3/Nodo.h
@@ -0,0 +1,36 @@
+#ifndef NODO_H
+#define NODO_H
+
+#include<iostream>
+#include<list>
+#include"Vertice.h"
+
+using namespace std;
+
+template<class T>
+class Nodo
+{
+    private:
+        Vertice<T> *vertice;
+        list<Vertice<T>*> listaAdj;
+
+    public:
+    Nodo(Vertice<T> *vertice){this->vertice = vertice;}
+
+    //Metodi di get
+    Vertice<T> *getVertice(){return vertice;}
+    list<Vertice<T>*> getListAdj(){return listaAdj;}
+
+    void append(Vertice<T> *vertice){listaAdj.push_back(vertice);}
+
+    friend ostream& operator<<(ostream &out, const Nodo<T> &obj)
+    {
+        out<<*obj.vertice<<"-->";
+        for(auto i:obj.listaAdj)
+            out<< *i << "-->";
+        out<<"NULL";
+        return out;
+    }
+};
+
+#endif
