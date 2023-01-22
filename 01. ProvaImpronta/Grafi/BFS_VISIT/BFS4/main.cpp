cd3841542371f372f84f0171b9f61aa7810ddf0f
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-01-22 13:12:25 +0100

    commit

--- 01. ProvaImpronta/Grafi/BFS_VISIT/BFS4/main.cpp
+++ 01. ProvaImpronta/Grafi/BFS_VISIT/BFS4/main.cpp
@@ -1,17 +1,42 @@
-
-#include<iostream>
-#include<string>
-#include<queue>
-
-#include"Vertice.h"
-#include"Nodo.h"
-#include"GrafoOrientato"
+#include <iostream>
+#include <string>
+#include <queue>
+#include "Nodo.h"
+#include "GrafoOrientato.h"
+#include "Vertice.h"
 
 using namespace std;
 
 int main(){
 
-    
+    GrafoOrientato<string> grafo;
+
+    Vertice<string> a("A");
+    Vertice<string> b("B");
+    Vertice<string> c("C");
+    Vertice<string> d("D");
+    Vertice<string> e("E");
+    Vertice<string> f("F");
+
+    grafo.addNodo(Nodo<string>(&a));
+    grafo.addNodo(Nodo<string>(&b));
+    grafo.addNodo(Nodo<string>(&c));
+    grafo.addNodo(Nodo<string>(&d));
+    grafo.addNodo(Nodo<string>(&e));
+    grafo.addNodo(Nodo<string>(&f));
+
+    grafo.addArco(0, &b);         // 0|A|--->|B|   |--->|E|   |--->NULL
+    grafo.addArco(0, &e);         // 1|B|--->|C|   |--->NULL
+    grafo.addArco(1, &c);         // 2|C|--->|D|   |--->|E|   |--->NULL
+    grafo.addArco(2, &d);         // 3|D|--->NULL
+    grafo.addArco(2, &e);         // 4|E|--->|F|   |--->NULL
+    grafo.addArco(4, &f);         // 5|F|--->NULL
+
+    queue<Vertice<string>*> queue;
+    queue.push(&a);
+    grafo.BFS(&a);
+    cout<<endl<<"BFS:"<<endl;
+    cout << grafo << endl;
 
     return 0;
 }
