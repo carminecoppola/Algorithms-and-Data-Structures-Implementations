4111f4518816d9fad8cbc096ca3dad104e2020ae
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-01-22 18:58:57 +0100

    heapsort su file

---
+++ 01. ProvaImpronta/File/HeapSort_con_File/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<string>
+
+#include"HeapSort.h"
+
+using namespace std;
+
+int main(){
+
+    string heap = "heap.txt";
+    fstream myFile;
+    string file;
+    vector<int> vect;
+
+    myFile.open(heap.c_str(),ios::in);
+
+    while(getline(myFile,file))
+        vect.push_back(stof(file));     //stof mi permette di convertire le stringhe in numeri
+
+    for(auto i:vect)
+        cout<<i<<endl;
+
+    //Applico HEAPSORT
+
+    cout<<endl<<"File ordinato: "<<endl;
+    HeapSort<int> tree = HeapSort<int>(vect);
+    tree.heapSort();
+    tree.printArray();
+
+    myFile.close();
+
+    return 0;
+}
