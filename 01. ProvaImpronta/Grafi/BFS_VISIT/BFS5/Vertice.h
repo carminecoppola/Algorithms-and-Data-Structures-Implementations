3502f21d1c74d77767258d225b19e639b9e20156
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-01-30 13:08:20 +0100

    commit

---
+++ 01. ProvaImpronta/Grafi/BFS_VISIT/BFS5/Vertice.h
@@ -0,0 +1,72 @@
+#ifndef VERTICE_H 
+#define VERTICE_H
+
+#include<iostream>
+#include<limits>
+#include<string>
+
+enum class Color{
+    WHITE,GRAY,BLACK
+
+};
+
+using namespace std;
+
+template<class T>
+class Vertice{
+
+    private:
+        T value;
+        Color colore;
+        Vertice<T> *predecessore;
+        int distanza;
+
+    public:
+        Vertice(T value);
+
+        //Metodi di set
+        void setValue(T value){this->value = value;}
+        void setColor(Colore colore){this->colore = colore;}
+        void setPredecessore(Vertice<T> *predecessore){this->predecessore = predecessore;}
+        void setDistanza(int distanza){this->distanza = distanza;}
+
+        //Metodi di get
+        T getValue(){return value;}
+        Color getColor(){return colore;}
+        Vertice<T> *getPredecessore(){return predecessore;}
+        int getDistanza(){return distanza;}
+
+        //Overload
+        friend ostream& operator<<(ostream& out, const Vertice<T> &obj)
+        {
+            string c;
+            switch(obj.colore)
+            {
+                case: Color::WHITE:
+                    c = "WHITE";
+                    break;
+                case: Color::GRAY:
+                    c = "GRAY";
+                    break;
+                case: Color::BLACK:
+                    c = "BLACK";
+                    break;
+                default:
+                    break;
+            }
+            out<<"Valore: "<<obj.value <<" Colore: "<<c;
+            return out;
+        }
+
+};
+
+template<class T> Vertice<T>::Vertice(T value)
+{
+    this->value = value;
+    colore = Color::WHITE;
+    predecessore = nullptr;
+    distanza = 0;
+}
+
+
+#endif
