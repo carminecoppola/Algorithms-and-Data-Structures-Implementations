893eb4cd55597cedcfc25af20f199454917802af
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-02-22 17:01:16 +0100

    completo

---
+++ 01. ProvaImpronta/Grafi/DFS_VISIT/DFS8/GrafoOrientato.h
@@ -0,0 +1,145 @@
+#ifndef GRAFOORIENTATO_H
+#define GRAFOORIENTATO_H
+
+#include"Vertice.h"
+#include"Nodo.h"
+
+#include<iostream>
+#include<list>
+#include<limits>
+#include<stack>
+#include<queue>
+#include<vector>
+#include<sstream>
+#include<fstream>
+
+using namespace std;
+
+template<class T>
+class GrafoOrientato
+{
+    private:
+        vector<Nodo<T>> grafo;
+        list<Vertice<T>*> getListAdj(Vertice<T> *vertice);
+
+        void DFS_VISIT(Vertice<T>* u);
+        queue<T> coda;
+        int time;
+    public:
+
+        queue<T> getCoda(){return coda;}
+
+        int searchVertice(Vertice<T> *v);
+        Vertice<T> *getIndirizzoVertice(T value);
+
+        void addNodo(Nodo<T> nodo){grafo.push_back(nodo);}
+        void addArco(Vertice<T> *v1, Vertice<T> *v2);
+
+        void DFS();
+
+        //Overload
+        friend ostream& operator<<(ostream& out, GrafoOrientato<T>& obj)
+        {
+            for(auto i:obj.grafo)
+                out<<i<<endl;
+            return out;
+        }
+
+};
+
+//SearchVertice
+template<class T> int GrafoOrientato<T>::searchVertice(Vertice<T> *v)
+{
+    for(int i = 0; i < grafo.size() ; i++)
+    {
+        if(this->grafo.at(i).getVertice()->getValue() == v->getValue())
+            return i;
+    }
+    return -1;
+}
+
+//GetIndirizzo
+template<class T> Vertice<T>* GrafoOrientato<T>::getIndirizzoVertice(T value)
+{
+    for(auto i:grafo)
+    {
+        if(i.getVertice()->getValue() == value)
+            return i.getVertice();
+    }
+    return nullptr;
+}
+
+//GetListADJ
+template<class T> list<Vertice<T>*> GrafoOrientato<T>::getListAdj(Vertice<T> *vertice)
+{
+    for(auto i:grafo)
+    {
+        if(i.getVertice()== vertice)
+            return i.getLista();
+    }
+    return grafo.at(0).getLista();
+}
+
+//AddArco
+template<class T> void GrafoOrientato<T>::addArco(Vertice<T> *v1, Vertice<T> *v2)
+{
+    int indice = searchVertice(v1);
+    grafo.at(indice).append(v2);
+}
+
+//DFS
+template<class T> void GrafoOrientato<T>::DFS()
+{
+    for(auto u:grafo)
+    {
+        u.getVertice()->setColore(Color::WHITE);
+        u.getVertice()->setPredecessore(nullptr);
+    }
+    time = 0;
+
+    for(auto u:grafo)
+    {
+        if(u.getVertice()->getColore() == Color::WHITE)
+            DFS_VISIT(u.getVertice());
+    }
+}
+
+//DFS_VISIT
+template<class T> void GrafoOrientato<T>::DFS_VISIT(Vertice<T>* u)
+{
+    ofstream fileOut;
+    string file2 = "Output.txt";
+
+    fileOut.open(file2);
+
+    u->setColore(Color::GRAY);
+    u->setTempoInizio(time++);
+
+    list<Vertice<T>*> adj = getListAdj(u);
+
+    for(auto v:adj)
+    {
+        if(v->getColore() == Color::WHITE)
+        {
+            v->setPredecessore(u);
+            DFS_VISIT(v);
+        }
+    }
+    u->setColore(Color::BLACK);
+    u->setTempoFine(time++);
+
+
+    coda.push(u->getValue());
+    
+    queue<int> coda_locale = coda;
+    /*il contenuto del file di output sarà una lista di valori,
+      uno per riga, che indicano i vertici visitati in ordine di visita.*/
+    while (!coda_locale.empty())
+    {
+        fileOut <<"Vertice: " << coda_locale.front() <<endl;
+        coda_locale.pop();
+    }
+    fileOut.close();
+}
+
+#endif
---
+++ 01. ProvaImpronta/Grafi/DFS_VISIT/DFS8/main.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<fstream>
+
+#include"Vertice.h"
+#include"Nodo.h"
+#include"GrafoOrientato.h"
+
+using namespace std;
+
+int main(){
+
+    GrafoOrientato<int> grafo;
+
+    ifstream file;
+    string myFile = "GTR.txt";
+    string line;
+
+    file.open(myFile);
+
+    int i = 0, num_vert = 0, num_archi = 0;
+    int vert, arco;
+
+    while (getline(file,line))
+    {
+        stringstream lineIn(line);
+
+        if(i == 0)
+        {
+            lineIn >> num_vert;
+            lineIn >> num_archi;
+            i++;
+        }
+
+        else
+        {
+            lineIn >> vert;
+            lineIn >> arco; 
+
+            Vertice<int> *vertice1 = new Vertice<int>(vert);
+            Vertice<int> *vertice2 = new Vertice<int>(arco);
+
+            if(grafo.searchVertice(vertice1) == -1)
+                grafo.addNodo(vertice1);
+            if(grafo.searchVertice(vertice2) == -1)
+                grafo.addNodo(vertice2);
+            grafo.addArco(grafo.getIndirizzoVertice(vert),grafo.getIndirizzoVertice(arco));
+            
+        }
+    }
+    
+    file.close();
+
+    cout<<"Grafo: "<<endl;
+    cout<<grafo<<endl;
+    grafo.DFS();
+    cout<<"Grafo dopo DFS: "<<endl;
+    cout<<grafo<<endl;
+
+    return 0;
+}
